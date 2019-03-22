675e9b59f98e0bf7be4aff6885190c3c76f19ce3
4d55397500 <4d55397500@users.noreply.github.com>
2019-03-22 00:00:16 -0700

    finish local search and api call

--- src/main/kotlin/MinHash.kt
+++ src/main/kotlin/MinHash.kt
@@ -16,18 +16,7 @@ import org.apache.beam.sdk.transforms.*
 import org.apache.beam.sdk.values.KV
 import org.apache.beam.sdk.values.PCollection
 import org.apache.beam.sdk.values.PCollectionList
-import java.util.*
 
-
-const val BQ_PROJECT = ""
-const val BQ_DATASET = ""
-const val BQ_MINHASHES_TABLE = "minhashes"
-const val BQ_HASHMAP_TABLE = "hashmap"
-
-
-const val LARGE_PRIME = 4294967311
-val RNG = Random(System.currentTimeMillis())
-val MAX_BYTE = Math.pow(2.0, 32.0).toInt()
 
 
 fun runPipelineMockDocs() {
